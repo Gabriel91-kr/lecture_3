1544680a936e0e65356e3a95104eb70d840280d2
Your Name <you@example.com>
2020-07-16 12:40:38 +0900

    remain_date_input()

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import func_module
+import func_module_as
 
 # func_module.module_show()
 
-nowdate = func_module.date_now()
-now_year = nowdate.year
-date_today = '{}년 {}월 {}일'.format(nowdate.year , nowdate.month ,nowdate.day)
+# nowdate = func_module.date_now()
+# now_year = nowdate.year
+# date_today = '{}년 {}월 {}일'.format(nowdate.year , nowdate.month ,nowdate.day)
 # print(nowdate)
-print(date_today)
+# print(date_today)
 
-X_mas = nowdate.replace(month = 12, day=25)
+# X_mas = nowdate.replace(month = 12, day=25)
 
-date_xmas = '{}년 {}월 {}일'.format(X_mas.year , X_mas.month ,X_mas.day)
-print(date_xmas)
+# date_xmas = '{}년 {}월 {}일'.format(X_mas.year , X_mas.month ,X_mas.day)
+# print(date_xmas)
+
+
+ndate = func_module_as.date_now()
+print(ndate)
+
+func_module_as.remain_date()
